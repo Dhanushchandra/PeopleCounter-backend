3447d8a432fd3a75efadc0f70a04a6f7512ecc91
Dhanush chandra <dhanushbharath9@gmail.com>
2022-05-18 22:34:36 +0530

    backend api working.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.post("/api/count", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
+app.listen(5000);
