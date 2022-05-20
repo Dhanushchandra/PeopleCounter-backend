addb5c4cc78764328637aba1fe6a89cf95cc1a3d
Dhanush chandra <dhanushbharath9@gmail.com>
2022-05-20 23:16:22 +0530

    created post and get api able to get data.

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
-
 app.use(bodyParser.json());
-app.post("/api/count", (req, res) => {
-  console.log(req.body);
-  res.send("OK");
+
+var inup = 0;
+var indown = 0;
+var result;
+
+app.post("/api/post/count", (req, res) => {
+  var { up, down } = req.body;
+
+  if (req.body.up == undefined) {
+    console.log("called UNDEFINED");
+    inup = inup;
+  }
+
+  if (req.body.up) {
+    console.log("called NUMBER");
+    inup = inup + 1;
+  }
+
+  if (req.body.down == undefined) {
+    console.log("called UNDEFINED");
+    indown = 0;
+  }
+
+  if (req.body.down) {
+    console.log("called NUMBER");
+    indown = 1;
+  }
+
+  console.log("inup: " + inup);
+  console.log("indown: " + indown);
+  result = inup - indown;
+  console.log("result: " + result);
+
+  if (!result <= 0) {
+    inup = result;
+  }
+  if (result <= 0) {
+    inup = 0;
+  }
+  console.log("FINAL= " + inup);
+  res.send("ok");
+});
+
+app.get("/api/count", (req, res) => {
+  res.send(inup.toString());
 });
 
 app.listen(4000);
